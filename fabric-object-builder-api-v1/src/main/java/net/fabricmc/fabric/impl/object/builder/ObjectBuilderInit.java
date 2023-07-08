40d050526b9a2c44546d8f49a1500516952ad21f
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-07-08 15:34:48 +0300

    Make boat type registry not sync when empty

--- fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/impl/object/builder/ObjectBuilderInit.java
+++ fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/impl/object/builder/ObjectBuilderInit.java
@@ -18,11 +18,12 @@ package net.fabricmc.fabric.impl.object.builder;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistrySyncOption;
 import net.fabricmc.fabric.api.object.builder.v1.entity.boat.FabricBoatType;
 
 public final class ObjectBuilderInit implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		DynamicRegistries.registerSynced(FabricBoatType.REGISTRY_KEY, FabricBoatType.CODEC, FabricBoatType.NETWORK_CODEC);
+		DynamicRegistries.registerSynced(FabricBoatType.REGISTRY_KEY, FabricBoatType.CODEC, FabricBoatType.NETWORK_CODEC, DynamicRegistrySyncOption.SKIP_WHEN_EMPTY);
 	}
 }
